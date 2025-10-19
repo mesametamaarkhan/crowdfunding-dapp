95c06c02dbc3db47af7f09585368c8008307ea43
Mesam E Tamaar Khan <mesamtamaark@gmail.com>
2025-10-19 17:54:45 +0500

    removed postcss stuff

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-export const KYC_CONTRACT_ADDRESS = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
-export const CROWDFUND_CONTRACT_ADDRESS = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+export const KYC_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CROWDFUND_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 import KYCRegistry from "../../artifacts/contracts/KYCRegistry_MesamTamaarKhan.sol/KYCRegistry_MesamTamaarKhan.json";
 import Crowdfunding from "../../artifacts/contracts/Crowdfunding_MesamTamaarKhan.sol/Crowdfunding_MesamTamaarKhan.json";
